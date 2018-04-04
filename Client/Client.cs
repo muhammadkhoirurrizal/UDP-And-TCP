cdcc68a054d5f3f3382cef368c3b5e7cce6501db
DESKTOP-CE05QUU\M Khoirur Rizal <rizalkhoirur96@gmail.com>
2018-04-04 12:03:59 +0700

    UDP And TCP Chatting

---
+++ Client/Client.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            var udpClient = new UdpClient(8888);
+
+            var ServerEP = new IPEndPoint(IPAddress.Any, 0);
+            var ServerRequestData = udpClient.Receive(ref ServerEP);
+            var ServerRequest = Encoding.ASCII.GetString(ServerRequestData);
+            Console.WriteLine("Recived {0}", ServerRequest);
+
+            try
+            {
+                TcpClient tcpClient = new TcpClient(ServerEP.Address.ToString(), 4444);
+                Console.WriteLine("Connected to server.");
+                Console.WriteLine("");
+
+                Thread thread = new Thread(Read);
+                thread.Start(tcpClient);
+
+                StreamWriter sWriter = new StreamWriter(tcpClient.GetStream());
+
+                while (true)
+                {
+                    if (tcpClient.Connected)
+                    {
+                        string input = Console.ReadLine();
+                        sWriter.WriteLine(input);
+                        sWriter.Flush();
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Read(object obj)
+        {
+            TcpClient tcpClient = (TcpClient)obj;
+            StreamReader sReader = new StreamReader(tcpClient.GetStream());
+
+            while (true)
+            {
+                try
+                {
+                    string message = sReader.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ ServerUDPTCP/Server.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerUDPTCP
+{
+    class Server
+    {
+        private static TcpListener tcpListener;
+        private static List<TcpClient> tcpClientsList = new List<TcpClient>();
+
+        static int Mulai = 0;
+
+        static void Main(string[] args)
+        {
+            Thread WantClient = new Thread(Send);
+            WantClient.Start();
+
+            tcpListener = new TcpListener(IPAddress.Any, 4444);
+            tcpListener.Start();
+
+            Console.WriteLine("Server started");
+
+            while (true)
+            {
+                if (Mulai <= 2)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    tcpClientsList.Add(tcpClient);
+                    Thread thread = new Thread(ClientListener);
+                    thread.Start(tcpClient);
+                    Mulai++;
+                    if (Mulai ==2)
+                    {
+                        WantClient.Abort();
+                    }
+                }
+            }
+        }
+
+        public static void ClientListener(object obj)
+        {
+            TcpClient tcpClient = (TcpClient)obj;
+            StreamReader reader = new StreamReader(tcpClient.GetStream());
+
+            Console.WriteLine("Client connected");
+
+            while (true)
+            {
+                string message = reader.ReadLine();
+                Console.WriteLine(message);
+                BroadCast(message, tcpClient);
+            }
+        }
+
+        public static void BroadCast(string msg, TcpClient Client)
+        {
+            foreach (TcpClient client in tcpClientsList)
+            {
+                if (client != Client)
+                {
+                    StreamWriter sWriter = new StreamWriter(client.GetStream());
+                    sWriter.WriteLine(msg);
+                    sWriter.Flush();
+                }
+            }
+        }
+
+        static void Send()
+        {
+            //Mengirim ke Semua Jaringan
+            var Server = new UdpClient();
+            var RequestData = Encoding.ASCII.GetBytes("Host Created");
+            var ClientEP = new IPEndPoint(IPAddress.Any, 0);
+
+            while (true)
+            {
+                Server.EnableBroadcast = true;
+                Server.Send(RequestData, RequestData.Length, new IPEndPoint(IPAddress.Broadcast, 8888));
+            }
+        }
+    }
+}
